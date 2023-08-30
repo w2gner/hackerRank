749d03f9bf3e3f4a80636bb229472323e9ec8571
Wagner <wagnerghedin10@gmail.com>
2023-08-29 22:47:21 -0300

    solved simple-array-sum

---
+++ simple-array-sum.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'simpleArraySum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY ar as parameter.
+#
+
+def simpleArraySum(ar):
+    return int(math.fsum(ar))
+
+if __name__ == '__main__':
+    fptr = open('./birthday-cake-candles.txt', 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
